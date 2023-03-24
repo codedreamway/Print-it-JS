4a7277c4f043fde0c27a7af8de70ac04cce2eb3a
codedreamway <richiedavy23@gmail.com>
2023-03-24 12:07:20 +0100

    MISE A JOUR DU CARROUSSEL.

--- assets/script.js
+++ assets/script.js
@@ -1,115 +1,115 @@
-const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
+const slides = [{
+    "image": "slide1.jpg",
+    "tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
+  },
+  {
+    "image": "slide2.jpg",
+    "tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+  },
+  {
+    "image": "slide3.jpg",
+    "tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
+  },
+  {
+    "image": "slide4.png",
+    "tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
+  }
 ]
+console.log(slides);
 
-var leftArrow = document.querySelector(".arrow_left");
-var rightArrow = document.querySelector(".arrow_right")
-var imageList = document.querySelectorAll(".banner-img");
-var captionList = document.querySelectorAll("#banner p");
-var dotList = document.querySelectorAll(".dot");
+var tagLine = document.querySelector('#banner p');
+console.log(tagLine);
+var img = document.querySelector(".banner-img");
+console.log(img);
 
-// Transforme les nœuds NodeList en tableaux
-var images = Array.from(imageList);
-var captions = Array.from(captionList);
-var dots = Array.from(dotList);
 
-// Initialise l'indice de l'image active actuelle à 0
-var currentImage = 0;
 
-// Met à jour l'affichage pour l'image active
-function updateImage() {
-  // Cache toutes les images et les légendes
-  images.forEach(function(image) {
-    image.style.display = "none";
-  });
-  captions.forEach(function(caption) {
-    caption.style.display = "none";
-  });
 
-  // Affiche l'image active et la légende correspondante
-  images[currentImage].style.display = "block";
-  captions[currentImage].innerHTML = slides[currentImage].tagLine;
-  captions[currentImage].style.display = "block";
+var leftArrow = document.querySelector('.arrow_left');
+console.log(leftArrow);
 
-  // Met à jour l'indicateur de diapositive correspondant
-  dots.forEach(function(dot) {
-    dot.classList.remove("dot_selected");
-  });
-  dots[currentImage].classList.add("dot_selected");
-}
+leftArrow.addEventListener('click', leftClick);
 
-// Passe à l'image suivante
-function nextImage() {
-  // Incrémente l'indice de l'image active actuelle
-  currentImage++;
 
-  // Si on a atteint la fin de la liste des images, on revient au début
-  if (currentImage >= images.length) {
-    currentImage = 0;
-  }
+function leftClick() {
+  dot[count].classList.remove('dot_selected');
 
-  // Met à jour l'affichage pour l'image active
-  updateImage();
-}
+  if (count > 0) {
+    count--;
 
-// Passe à l'image précédente
-function prevImage() {
-  // Décrémente l'indice de l'image active actuelle
-  currentImage--;
-
-  // Si on a atteint le début de la liste des images, on revient à la fin
-  if (currentImage < 0) {
-    currentImage = images.length - 1;
-  }
-
-  // Met à jour l'affichage pour l'image active
-  updateImage();
-}
-
-// Ajoute des écouteurs d'événements aux flèches de gauche et de droite
-leftArrow.addEventListener("click", function() {
-  // Si on est à la première image et qu'on clique à gauche,
-  // on affiche la dernière image et on sélectionne le dernier point
-  if (currentImage === 0) {
-    currentImage = images.length - 1;
-    dots.forEach(function(dot) {
-      dot.classList.remove("dot_selected");
-    });
-    dots[dots.length - 1].classList.add("dot_selected");
   } else {
-    // Sinon, on passe simplement à l'image précédente
-    currentImage--;
+    count = nombreBullet - 1
   }
 
-  // Met à jour l'affichage pour l'image active
-  updateImage();
-});
+  var recuperationImage = slides[count].image;
+  console.log(recuperationImage);
 
-rightArrow.addEventListener("click", function() {
-  // Si on est à la dernière image et qu'on clique à droite,
-  // on affiche la première image et on sélectionne le premier point
-  if (currentImage === images.length - 1) {
-    currentImage = 0;
-    dots.forEach(function(dot) {
-      dot.classList.remove("dot_selected");
-    });
-    dots[0].classList.add("dot_selected");
+  var recuperationTagline = slides[count].tagLine;
+  console.log(recuperationTagline);
+
+  var changerImage = img.src = "./assets/images/slideshow/" + recuperationImage;
+  console.log(changerImage);
+
+  var changerTexte = tagLine.innerHTML = recuperationTagline;
+  console.log(changerTexte);
+
+
+  dot[count].classList.add('dot_selected');
+
+
+};
+
+
+//===============
+
+
+var rightArrow = document.querySelector('.arrow_right');
+console.log(rightArrow);
+
+rightArrow.addEventListener('click', rightClick);
+
+function rightClick() {
+  dot[count].classList.remove('dot_selected');
+
+  if (count < nombreBullet - 1) {
+    count++;
   } else {
-    // Sinon, on passe simplement
+    count = 0
   }
-});
+
+  var recuperationImage = slides[count].image;
+  console.log(recuperationImage);
+
+  var recuperationTagline = slides[count].tagLine;
+  console.log(recuperationTagline);
+
+  var changerImage = img.src = "./assets/images/slideshow/" + recuperationImage;
+  console.log(changerImage);
+
+  var changerTexte = tagLine.innerHTML = recuperationTagline;
+  console.log(changerTexte);
+
+  dot[count].classList.add('dot_selected');
+};
+
+
+
+
+
+
+
+
+
+
+
+// rappeler mes bullets
+
+var dot = document.querySelectorAll('.dot')
+console.log(dot);
+
+// Déterminer la longueur des bullets
+var nombreBullet = dot.length;
+console.log(nombreBullet);
+
+// initialiser la valeur de la position   à Zéro
+let count = 0;
